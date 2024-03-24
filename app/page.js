5ff2cd5740a26d8ec224e822ec923e1437deb20f
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-03-24 18:10:15 +0100

    Section 'about' bug fix

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useAppContext } from "./components/Context";
 import Landing from "./sections/Landing";
-import About from "./sections/about";
 import Work from "./sections/Work";
+import About from "./sections/About";
 
 export default function Home() {
   const { updateActiveSection } = useAppContext();
