48a8fd68a9ebb377a76d9193906c40fc77db3484
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-04-22 15:51:44 +0200

    Metadata fix and favicon attempt

--- app/page.js
+++ app/page.js
@@ -1,15 +1,14 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useAppContext } from "./components/Context";
-import Head from "next/head";
+
 import Landing from "./sections/Landing";
 import Projects from "./sections/Projects";
 import Footer from "./components/Footer";
 
 export default function Home() {
   const { updateActiveSection } = useAppContext();
-  const [isFirstScroll, setIsFirstScroll] = useState(true);
 
   useEffect(() => {
     let home = document.getElementById("home");
@@ -37,21 +36,10 @@ export default function Home() {
   }, []);
 
   return (
-    <>
-      <main className="h-screen overflow-x-hidden snap-y snap-mandatory antialiased scroll-smooth scrollbar-hide">
-        <Head>
-          <title>Karli Christensen - Portfolio</title>
-          <meta
-            name="description"
-            content="Web Developer Portfolio for Karli Christensen"
-          />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Landing />
-        <Projects />
-        <Footer />
-      </main>
-    </>
+    <main className="h-screen overflow-x-hidden snap-y snap-mandatory antialiased scroll-smooth scrollbar-hide">
+      <Landing />
+      <Projects />
+      <Footer />
+    </main>
   );
 }
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -3,20 +3,9 @@ import Head from "next/head";
 
 function Projects() {
   return (
-    <>
-      <Head>
-        <title>Karli Christensen - Projects</title>
-        <meta
-          name="description"
-          content="Web Developer Portfolio for Karli Christensen"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="flex justify-center items-center w-full h-screen text-center bg-green-600/50 font-bold text-white">
-        WORK IN PROGRESS! <br /> GO AWAY!
-      </div>
-    </>
+    <div className="flex justify-center items-center w-full h-screen text-center bg-green-600/50 font-bold text-white">
+      WORK IN PROGRESS! <br /> GO AWAY!
+    </div>
   );
 }
 
