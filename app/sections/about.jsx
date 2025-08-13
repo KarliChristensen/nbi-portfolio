9c7ad6a1958ac8c7afaa345450ece7322171afa4
Karli Christensen <KarliChristensen@post.com>
2025-08-13 23:06:42 +0200

    Reworked function to const

--- app/sections/about.jsx
+++ app/sections/about.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
-function About({
+const About = ({
   navigateToSection,
   sectionStatus,
   isTransitioning,
   currentSection,
   ...restProps
-}) {
+}) => {
   return (
     <div
       id="about"
