226fa48341d4c5962f35b4985c6d9b79dec21682
Karli Christensen <KarliChristensen@post.com>
2025-08-13 23:02:31 +0200

    Extension resolution to deployment failure

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@
 
 import React from "react";
 import SpatialNavigation from "./components/SpatialNavigation";
-import Landing from "./sections/Landing";
-import Projects from "./sections/Projects";
-import About from "./sections/About";
+import Landing from "./sections/Landing.jsx";
+import Projects from "./sections/Projects.jsx";
+import About from "./sections/About.jsx";
 
 export default function Home() {
   return (
